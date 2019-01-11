2ef252a23caed99079fc70bda0d64bea80241f80
Rafail Kyriakopoulos <slave@slave>
2019-01-11 03:35:14 +0200

    initialize git

---
+++ fuelProject/src/main/java/teilar/cs4414202/service/PriceDataService.java
@@ -0,0 +1,33 @@
+package teilar.cs4414202.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import teilar.cs4414202.repository.dto.FuelTypeDTO;
+import teilar.cs4414202.exception.ResourceNotFoundException;
+import teilar.cs4414202.model.PriceData;
+import teilar.cs4414202.repository.PriceDataRepository;
+
+@Service
+public class PriceDataService {
+	@Autowired
+	private PriceDataRepository priceDataRepository;
+	
+	public boolean existsById(Long productID) {
+		return priceDataRepository.existsById(productID);
+	}
+	
+	public void save(PriceData priceData) {
+		priceDataRepository.save(priceData);
+	}
+
+	public List<FuelTypeDTO> getFuelTypes() {
+		return priceDataRepository.getFuelTypes();
+	}
+	public PriceData getPriceDataById(Long priceDataId) {
+		return priceDataRepository.findById(priceDataId).orElseThrow(()->new ResourceNotFoundException("PriceData", "priceData",priceDataId));
+	}
+	
+}
